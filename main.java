69421af158a7632815fb7f3c6863fa42ce69736b
Linda John <lindajohn2202@gmail.com>
2021-12-01 17:54:40 +0530

    Adding comments to the code

--- main.java
+++ main.java
@@ -1,11 +1,11 @@
 public class main{
 	public static void main(String args[]){
-		System.out.println("Hello World");	
+		System.out.println("Hello World"); //printing a statement
 		System.out.println("Printing a new line");
 		int val = 5;
-		int sum = val+val;
+		int sum = val+val; //adding the values and storing it to a variable
 		System.out.println(sum);
-		if(sum<10){
+		if(sum<10){  //if..else used for conditional approach
 			System.out.println("Result is less than 10"); }
 		else{
 			System.out.println("Result is greater than or equal to 10"); }

739bf825ea0f0a80fd54355101d91cfef1bab256
Linda John <lindajohn2202@gmail.com>
2021-12-01 17:53:16 +0530

    Adding conditional statements

--- main.java
+++ main.java
@@ -3,7 +3,12 @@ public class main{
 		System.out.println("Hello World");	
 		System.out.println("Printing a new line");
 		int val = 5;
-		System.out.println(val+val);
+		int sum = val+val;
+		System.out.println(sum);
+		if(sum<10){
+			System.out.println("Result is less than 10"); }
+		else{
+			System.out.println("Result is greater than or equal to 10"); }
 
  	}
 }

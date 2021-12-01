ee2be07133616885ce462659788a047328bf9f8f
Linda John <lindajohn2202@gmail.com>
2021-12-01 17:50:01 +0530

    Adding two numbers

--- main.java
+++ main.java
@@ -2,6 +2,8 @@ public class main{
 	public static void main(String args[]){
 		System.out.println("Hello World");	
 		System.out.println("Printing a new line");
+		int val = 5;
+		System.out.println(val+val);
 
  	}
 }

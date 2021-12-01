054e96baa46e4f5a77c618eec2bf1a075b3434c4
Linda John <lindajohn2202@gmail.com>
2021-12-01 17:48:01 +0530

    Printing a new line

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 public class main{
 	public static void main(String args[]){
-		System.out.println("hello world");
+		System.out.println("Hello World");	
+		System.out.println("Printing a new line");
 
  	}
 }
